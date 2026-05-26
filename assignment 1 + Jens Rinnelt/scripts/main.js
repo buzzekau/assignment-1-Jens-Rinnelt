74e88a80457d2ecafca7c915e0758765d94ffa06
buzzekau <jens.rinnelt@gmx.de>
2026-05-26 16:09:28 +0200

    Add files via upload

---
+++ assignment 1 + Jens Rinnelt/scripts/main.js
@@ -0,0 +1,15 @@
+function divide() {
+    console.log("divide");
+}
+
+let foo = 0;
+
+let bar = 0;
+
+let result = foo / bar;
+
+if (isNaN(result)){
+    alert("You can't divide us!");
+} else {
+    console.log(result);
+}
